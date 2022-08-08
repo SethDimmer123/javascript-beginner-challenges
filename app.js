8bab1c7421ac11114a2c96fb9fae40c4ddf1d7a0
Seth DImmer <sethdimmer1@gmail.com>
2022-08-08 11:54:33 -0400

    initial commit

--- app.js
+++ app.js
@@ -1,1 +1,18 @@
-console.log('it worked')
+// Question 1. Return the Sum of Two Numbers
+
+// Create a function that takes two arguements as functions and return their sum.
+
+
+function addition(num1,num2) {
+    return num1 + num2
+}
+
+console.log(addition(3,5));
+
+// with a function you have to return something if i want to console log it
+
+
+
+
+
+
