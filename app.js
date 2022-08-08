7a258b3240e6d31aadda538b0ec74aca2b98cc95
Seth DImmer <sethdimmer1@gmail.com>
2022-08-08 17:51:13 -0400

    initial commit

--- app.js
+++ app.js
@@ -57,6 +57,13 @@ console.log(calcPerimeter(6,7));
 // REMEMBER PEMDAS
 
 
+// question 4. Calculate the Area of a Triangle
+
+// Write a function that takes the base and height of a triangle and return its area.
+
+
+
+
 
 
 
